55a7a5f9ded9a514cd14b0a519c098fd088136f7
Morgan Fudge <mln_mf@hotmail.com>
2018-05-20 21:27:23 -0230

    Updated required arguments

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,11 @@
 let program = require('commander');
 let DigiStream = require('../src/DigiStream');
-let id, uuid, endpoint;
 
 program.option('-e, --endpoint [url]', 'Set data endpoint').parse(process.argv);
 
-id = program.args[0];
-uuid = program.args[1];
-endpoint = program.endpoint ? program.endpoint : 'localhost:3000';
+let id = program.args[0];
+let stream_id = program.args[1];
+let uuid = program.args[2];
+let endpoint = program.endpoint ? program.endpoint : 'localhost:3000';
 
-new DigiStream(id, uuid, endpoint);
+new DigiStream(id, stream_id, uuid, endpoint);
